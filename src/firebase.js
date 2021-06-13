e8b68a04dae319ce4d687fdbb4a1b56a33349d72
Kardev07 <77888951+Kardev07@users.noreply.github.com>
2021-06-13 23:37:50 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAw-SUntJgbHxG6vUu7V71FjwUy8PAlTTE",
+    authDomain: "tesla-clone-b2d93.firebaseapp.com",
+    projectId: "tesla-clone-b2d93",
+    storageBucket: "tesla-clone-b2d93.appspot.com",
+    messagingSenderId: "617198492082",
+    appId: "1:617198492082:web:c97c86b6977620fa95acab",
+    measurementId: "G-Y9QLFHCXBW"
+  };
